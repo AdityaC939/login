b4839a279ee059d804bef9287b33ec59fcd319f5
Utkarsh Choudhary <choudharyu2003@gmail.com>
2023-07-02 00:05:27 +0530

    rounded edges

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,21 +9,23 @@ const App = () => {
       <div className="imgcontainer">
         <img src={robot} alt="Avatar" className="avatar" />
       </div>
-
-      <div className="container">
+    <div className="container">
+      <div className="container1">
         <label htmlFor="uname"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="uname" required />
-
+      </div>
+      <div className="container2">
         <label htmlFor="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" required />
-
+      </div>
+      <div className="container3">
         <button type="submit">Login</button>
         <label>
           <input type="checkbox" defaultChecked name="remember" /> Remember me
         </label>
       </div>
-
-      <div className="container" style={{ backgroundColor: '#f1f1f1' }}>
+    </div>
+      <div className="cancelbtn" style={{ backgroundColor: '#f1f1f1' }}>
         <button type="button" className="cancelbtn">Cancel</button>
         <span className="psw"> <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> Forgot password?</a></span>
       </div>
